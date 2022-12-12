857df2bce9ac17795c92cef9c3d2a81a3371be2e
saikiranbembalge <saikiranbembalge123@gmail.com>
2022-12-12 06:20:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  Footer,
+  Blog,
+  Header,
+  Possibility,
+  WhatGPT3,
+  Feature,
+} from "./containers";
+
+import { CTA, Brand, Navbar } from "./components";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Feature />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
